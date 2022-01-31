d13cfe5256dc8b8ad10dec9ea128f33927265239
Pablo Garcia <pablo.garcia366@gmail.com>
2022-01-31 16:45:13 -0300

    add express validator and registry validations

--- __test__/UserRegister.spec.js
+++ __test__/UserRegister.spec.js
@@ -11,7 +11,7 @@ beforeEach(() => {
   return User.destroy({ truncate: true, cascade: true });
 });
 
-const postUser = ({ username = 'user1', email = 'user1@mail.com', password = 'Password' } = {}) => {
+const postUser = ({ username = 'user1', email = 'user1@mail.com', password = 'P4ssword' } = {}) => {
   return request(app).post('/api/1.0/users').send({
     username,
     email,
@@ -67,18 +67,27 @@ describe('User registry', () => {
     expect(response.body.validationErrors).not.toBeUndefined();
   });
 
-  it('returns username cannot be null when username is null', async () => {
-    const response = await postUser({ username: null });
-    expect(response.body.validationErrors.username).toBe('username cannot be null');
-  });
-
-  it('returns E-mail cannot be null when email is null', async () => {
-    const response = await postUser({ email: null });
-    expect(response.body.validationErrors.email).toBe('E-mail cannot be null');
-  });
-
   it('returns both errors if username and email are null', async () => {
     const response = await postUser({ email: null, username: null });
     expect(Object.keys(response.body.validationErrors)).toEqual(['username', 'email']);
+  });
+
+  it.each`
+    field         | value             | expected
+    ${'username'} | ${null}           | ${'username cannot be null'}
+    ${'username'} | ${'usr'}          | ${'Must have min 4 and max 32 characters'}
+    ${'username'} | ${'a'.repeat(33)} | ${'Must have min 4 and max 32 characters'}
+    ${'email'}    | ${null}           | ${'E-mail cannot be null'}
+    ${'email'}    | ${'fakeemail'}    | ${'E-mail is not valid'}
+    ${'password'} | ${null}           | ${'Password cannot be null'}
+    ${'password'} | ${'pass'}         | ${'Password must be at least 6 characters'}
+    ${'password'} | ${'alllowercase'} | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
+    ${'password'} | ${'ALLUPPERCASE'} | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
+    ${'password'} | ${'1234567'}      | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
+    ${'password'} | ${'lowerand123'}  | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
+    ${'password'} | ${'UPPER1234'}    | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
+  `('returns $expected when $field is $value', async ({ field, expected, value }) => {
+    const response = await postUser({ [field]: value });
+    expect(response.body.validationErrors[field]).toBe(expected);
   });
 });
--- src/user/user.router.js
+++ src/user/user.router.js
@@ -1,36 +1,37 @@
 const router = require('express').Router();
 const UserService = require('./UserService');
+const { check, validationResult } = require('express-validator');
 
-const validateUsername = (req, res, next) => {
-  const user = req.body;
-  if (!user.username) {
-    req.validationErrors = {
-      ...req.validationErrors,
-      username: 'username cannot be null',
-    };
+router.post(
+  '/api/1.0/users',
+  check('username')
+    .notEmpty()
+    .withMessage('username cannot be null')
+    .bail()
+    .isLength({ max: 32, min: 4 })
+    .withMessage('Must have min 4 and max 32 characters'),
+  check('email').notEmpty().withMessage('E-mail cannot be null').bail().isEmail().withMessage('E-mail is not valid'),
+  check('password')
+    .notEmpty()
+    .withMessage('Password cannot be null')
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters')
+    .bail()
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/)
+    .withMessage('Password must have at least 1 uppercase, 1 lowercase letter and 1 number'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const validationErrors = {};
+      errors.array().forEach((error) => {
+        validationErrors[error.param] = error.msg;
+      });
+      return res.status(400).json({ validationErrors });
+    }
+    await UserService.save(req.body);
+    res.send({ message: 'User created' });
   }
-  next();
-};
-
-const validateEmail = (req, res, next) => {
-  const user = req.body;
-  if (!user.email) {
-    req.validationErrors = {
-      ...req.validationErrors,
-      email: 'E-mail cannot be null',
-    };
-  }
-  next();
-};
-
-router.post('/api/1.0/users', validateUsername, validateEmail, async (req, res) => {
-  if (req.validationErrors) {
-    return res.status(400).json({
-      validationErrors: req.validationErrors,
-    });
-  }
-  await UserService.save(req.body);
-  res.send({ message: 'User created' });
-});
+);
 
 module.exports = router;
